4e484f1ac2eb823856c6b4da6f860e02660e7f01
Yash Babnai <yashbabani6900@gmail.com>
2021-06-23 15:07:16 +0530

    Fetch Post Comment

--- src/redux/ActionCreator.js
+++ src/redux/ActionCreator.js
@@ -2,17 +2,51 @@ import * as ActionType from './ActionTypes';
 
 import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (dishId, rating , author , comment)=>({
+export const addComment = (comment)=>({
     type :ActionType.ADD_COMMENT,
-    payload : {
-        dishId : dishId,
-        rating : rating,
-        author : author,
-
-
-        comment: comment
-    } 
+    payload : comment
 });
+
+export const postComment = (dishId, rating , author , comment)=> (dispatch) => {
+
+  const newComment = {
+    dishId: dishId,
+    rating : rating,
+    author : author,
+    comment : comment
+  }
+
+  newComment.date = new Date().toISOString();
+
+  return fetch(baseUrl + 'comments', {
+    method: 'POST',
+    body: JSON.stringify(newComment),
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    credentials: 'same-origin'
+    
+  })
+
+  .then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      var error = new Error('Error ' + response.status + ': ' + response.statusText);
+      error.response = response;
+      throw error;
+    }
+  },
+  error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+  })
+  .then(response => response.json())
+  .then(response => dispatch(addComment(response)))
+  .catch(error => {console.log('Post comments', error.message)
+  alert('Your Comment could not pe posted\nError: ' + error.message);  })
+
+}
 
 export const fetchDishes = () => (dispatch) => {
 
